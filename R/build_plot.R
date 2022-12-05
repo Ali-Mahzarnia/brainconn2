817263284fbb1e78266a6bd52cb5096304ad794c
Ali-Mahzarnia <69542146+Ali-Mahzarnia@users.noreply.github.com>
2022-12-05 13:20:43 -0500

    Update build_plot.R

--- R/build_plot.R
+++ R/build_plot.R
@@ -253,11 +253,11 @@ build_plot <- function(conmat, data, data.row=NULL, data.col=NULL, background, n
     annotation_custom(background, xmax = xmax ,xmin = xmin , ymax = ymax , ymin = ymin ) +
     geom_edge_link(aes(color = weight),
                            edge_width = edge.width,
-                           edge_alpha = edge.alpha) +
-      scale_color_viridis()+
+                          edge_alpha = edge.alpha) +
+    scale_edge_colour_distiller(palette = "RdPu")+
     coord_fixed(xlim = c(-70,70), ylim = c(-107,73))
     #print(attributes(layout))
-    cat("here17")
+    cat("here19")
   }
 
   if(directed == F && weighted==T && edge.color.weighted==T && label.edge.weight==T){
