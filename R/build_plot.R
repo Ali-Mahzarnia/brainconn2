0dba2d1d549563de89d34a849c0d8f6e9f4c26ff
Ali-Mahzarnia <69542146+Ali-Mahzarnia@users.noreply.github.com>
2022-12-05 15:02:55 -0500

    Update build_plot.R

--- R/build_plot.R
+++ R/build_plot.R
@@ -256,10 +256,10 @@ rwb=colorRampPalette(colors = c( "blue", "red"))
     geom_edge_link(aes(color = weight),
                            edge_width = edge.width,
                           edge_alpha = edge.alpha) +
-    scale_edge_color_distiller( rwbready , limits = c(min(conmat), max(conmat)))+
+    scale_edge_color_distiller( rwbready )+
     coord_fixed(xlim = c(-70,70), ylim = c(-107,73))
     #print(attributes(layout))
-    cat("here32")
+    cat("here33")
   }
 
   if(directed == F && weighted==T && edge.color.weighted==T && label.edge.weight==T){
