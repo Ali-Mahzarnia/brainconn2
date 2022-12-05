39d15bd5cd6425b70ee9518c715f0cae63db0e9c
Ali-Mahzarnia <69542146+Ali-Mahzarnia@users.noreply.github.com>
2022-12-05 11:51:40 -0500

    Update build_plot.R

--- R/build_plot.R
+++ R/build_plot.R
@@ -250,11 +250,13 @@ build_plot <- function(conmat, data, data.row=NULL, data.col=NULL, background, n
 
   if(directed == F && weighted==T && edge.color.weighted==T && label.edge.weight==F){
     p <- ggraph(layout, circular = FALSE) +
+     geom_col(position = "identity")+
     annotation_custom(background, xmax = xmax ,xmin = xmin , ymax = ymax , ymin = ymin ) +
     geom_edge_link(aes(colour=weight),
                            edge_width = edge.width,
                            edge_alpha = edge.alpha) +
     coord_fixed(xlim = c(-70,70), ylim = c(-107,73))
+    cat("here1")
   }
 
   if(directed == F && weighted==T && edge.color.weighted==T && label.edge.weight==T){
